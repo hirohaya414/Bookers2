ee1e324065dfde5d31adff8e7672d299da7ab245
vagrant <vagrant@localhost.localdomain>
2020-05-04 05:28:08 +0000

    [Add] トップ画面の作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,12 @@
+class BooksController < ApplicationController
+	before_action :authenticate_user!
+	
+	def new
+    end
+    def create
+    end
+    def index
+    end
+    def show
+    end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+	before_action :authenticate_user!
+	
+  def index
+
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def edit
+  end
+end
