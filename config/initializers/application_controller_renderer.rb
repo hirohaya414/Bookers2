25d6cd69f619868a4869a1da3916ca6e6471f1fd
vagrant <vagrant@localhost.localdomain>
2020-05-10 09:14:42 +0000

    [Fix] 画像の投稿

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '148c57c80588435e91a02e0826af83d8e4477ebfe89259fd2ba9d925c2a9341abc6fac64484b3affc98b16c63fb62d311f4000fa06b8c613e1fb8da452bfe3e8'
