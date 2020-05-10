1ea6fd2aa59a0dcc72e78c53b82990fbd746b3b6
vagrant <vagrant@localhost.localdomain>
2020-05-10 11:19:07 +0000

    [Fix] 投稿に失敗する

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    if book.save
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
       flash[:notice] = "You have created successfully "
-      redirect_to book_path(book)
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
     end
   end
 
