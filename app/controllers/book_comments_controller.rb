c86f0def7e418c5ebc4350af4a725291b4fc0994
vagrant <vagrant@localhost.localdomain>
2020-05-24 08:13:03 +0000

    [Update] コメント機能の非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,15 @@
 class BookCommentsController < ApplicationController
 	def create
-		book = Book.find(params[:book_id])
-		book_comment = current_user.book_comments.new(book_comment_params)
-		book_comment.book_id = book.id
-		book_comment.save
-		redirect_back(fallback_location: root_path)
+		@book = Book.find(params[:book_id])
+		@book_comment = current_user.book_comments.new(book_comment_params)
+		@book_comment.book_id = @book.id
+		@book_comment.save
 	end
 
 	def destroy
-		book_comment = BookComment.find(params[:book_id])
-	    book_comment.destroy
-	    redirect_back(fallback_location: root_path)
+		@book_comment = BookComment.find(params[:book_id])
+		@book = @book_comment.book
+	    @book_comment.destroy
 	end
 
 	private
